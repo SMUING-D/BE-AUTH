8a7777338dc753d13152a7a3eacac300b1937468
Jaeyoung Lim <limjyjustin@gmail.com>
2024-03-05 14:04:20 +0900

    fix: DTO 수정

--- src/main/java/dev/umc/auth/domain/user/converter/UserConverter.java
+++ src/main/java/dev/umc/auth/domain/user/converter/UserConverter.java
@@ -8,18 +8,17 @@ public class UserConverter {
     public static User toUser(UserRequest.JoinDto joinDto) {
         return User.builder()
                 .username(joinDto.getUsername())
+                .studentId(joinDto.getStudent_id())
                 .password(SecurityConfig.passwordEncoder().encode(joinDto.getPassword()))
                 .role("USER")
-                .studentId(joinDto.getStudent_id())
                 .major(joinDto.getMajor())
-                .subMajor(joinDto.getSub_major())
                 .email(joinDto.getEmail())
                 .nickname(joinDto.getNickname())
                 .desiredEmployment(joinDto.getDesired_employment())
                 .skill(joinDto.getSkill())
                 .grade(joinDto.getGrade())
                 .college(joinDto.getCollege())
-                .graduation(joinDto.getGraduation())
+                .undergraduateGraduate(joinDto.getUndergraduate_graduate())
                 .profileImg(joinDto.getProfile_img())
                 .build();
     }
--- src/main/java/dev/umc/auth/domain/user/dto/UserRequest.java
+++ src/main/java/dev/umc/auth/domain/user/dto/UserRequest.java
@@ -16,14 +16,13 @@ public class UserRequest {
         private Long student_id;
         private String password;
         private String major;
-        private String sub_major;
         private String email;
         private String nickname;
         private String desired_employment;
         private String skill;
         private Grade grade;
         private String college;
-        private UndergraduateGraduate graduation;
+        private UndergraduateGraduate undergraduate_graduate;
         private String profile_img;
     }
 }
